49b41123ddbf289fe3e6b5a2d58ba773dcb33066
Robin Cade <robin.cade54@gmail.com>
2022-11-07 11:38:32 +0000

    returns an array

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -7,15 +7,25 @@ describe('/cards', () => {
       return request(app).get('/cards').expect(200).then((response) => {
         expect(Array.isArray(response.body.cards)).toBe(true)
       })
-      })
+    })
+    // test("Should return a list of cards in the correct format", () => {
+    //   return request(app).get('/cards').expect(200).then((response) => {
+    //     expect(response.body.cards[0]).toEqual({
+    //         id: expect.any(String),
+    //         title: expect.any(String),
+    //         imageUrl: expect.any(String)
+            
+    //       })
+    //   })
+    // })
     })
 });
 
-test('returns matching card title', async () => {
-  const response = await request(app).get('/cards/card001')
+// test('returns matching card title', async () => {
+//   const response = await request(app).get('/cards/card001')
 
-  expect(response.status).toBe(200)
-  expect(response.body).toEqual(expect.objectContaining({
-    title: 'card 1 title',
-  }))
-})
+//   expect(response.status).toBe(200)
+//   expect(response.body).toEqual(expect.objectContaining({
+//     title: 'card 1 title',
+//   }))
+// })
--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
 /** @format */
 
 const express = require('express');
+const {getCards} = require('./controllers/card-controllers')
 
 const app = express();
+app.use(express.json())
 
 app.set('json spaces', 2);
 
-app.get('/cards', async () => {
-	// respond with a list of cards
-});
+app.get('/cards', getCards);
 
 app.get('/cards/:cardId/:sizeId?', () => {
 	// respond with card by id
