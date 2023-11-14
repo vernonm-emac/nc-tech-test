69cdcba3cb767d30f4196d99d228aa6822345143
adaml98 <47745325+adaml98@users.noreply.github.com>
2023-11-14 19:48:12 +0000

    Filter endpoint so it returns only relevant data

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -6,6 +6,25 @@ test('GET /cards returns status code 200', async () => {
     expect(response.status).toBe(200);
 });
 
+test('GET /cards returns list of cards with title, imageUrl and card_id', async () => {
+  const response = await request(app).get('/cards')
+  expect(response.body.cards).toEqual([{
+    "title": "card 1 title",
+    "imageUrl": "/front-cover-portrait-1.jpg",
+    "card_id": "card001"
+  },
+  {
+    "title": "card 2 title",
+    "imageUrl": "/front-cover-portrait-2.jpg",
+    "card_id": "card002"
+  },
+  {
+    "title": "card 3 title",
+    "imageUrl": "/front-cover-landscape.jpg",
+    "card_id": "card003"
+  }]);
+})
+
 test.skip('returns matching card title', async () => {
   const response = await request(app).get('/cards/card001')
   expect(response.status).toBe(200)
--- src/models/cards.models.ts
+++ src/models/cards.models.ts
@@ -3,5 +3,26 @@ import * as fs from "fs/promises";
 export const fetchCards = async () => {
     const cards = await fs.readFile(`${__dirname}/../data/cards.json`, "utf8");
     const parsedCards = JSON.parse(cards);
-    return parsedCards;
+
+    const templates = await fs.readFile(`${__dirname}/../data/templates.json`, "utf-8");
+    const parsedTemplates = JSON.parse(templates);
+
+    const transformedCards = [];
+    for (let i = 0; i < parsedCards.length; i++){
+        const matchingTemplate = parsedTemplates.find(template => template.id === parsedCards[i].pages[0].templateId);
+        const imageUrl = matchingTemplate.imageUrl;
+        transformedCards.push({
+            "title": parsedCards[i].title,
+            imageUrl,
+            "card_id": parsedCards[i].id,
+        })
+    }
+
+    // const transformedCards = parsedCards.map((card) => ({
+    //     "title": card.title,
+    //     "imageUrl": firstPageImage,
+    //     "card_id": card.id,
+    // }));
+
+    return transformedCards;
 }
