4f9c9620a7a2cced6da66d61248ba0b9c184fe43
Isaac-Sturtridge <eyes330@yahoo.co.uk>
2024-02-06 15:33:04 +0000

    consistent exporting practices

--- src/controllers/cards.controllers.ts
+++ src/controllers/cards.controllers.ts
@@ -1,9 +1,7 @@
 import { selectCards} from "../models/cards.models"
 
-export module CardControllers {
-    export async function getCards (req, res, next): Promise<any> {
+export async function getCards (req, res, next): Promise<any> {
         const cards = await selectCards()
-        
-        return res.status(200).send({cards})
-    }
+
+        return res.status(200).send({cards}).catch(next)
 }
