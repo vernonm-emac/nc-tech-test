d7a69f59f22e9be8125ec453d226f260bf749958
Bret <bret_gomes811@hotmail.co.uk>
2024-02-07 14:25:15 +0000

    update file name

--- src/models/cards.model.ts
+++ src/models/cards.model.ts
@@ -1,5 +1,5 @@
 import { readFileSync } from "fs";
-import { fetchImageUrlById } from "./template.model";
+import { fetchImageUrlById } from "./templates.model";
 
 type CardResponse = {
     title: string;
